a13b6b4bcc967803ed5e73bdca0ba9ca6c646bbc
stevenmontes <39229428+stevenmontes@users.noreply.github.com>
2018-06-15 17:40:22 -0600

    Implemeta el metodo equals en la clase Carta
    Se implementa las pruebas basados en el metodo equals de clase Carta, ademas de mejoras en la clase Carta y CartaTest

--- Examen1Patrones/src/cr/ac/cenfotec/logica/Carta.java
+++ Examen1Patrones/src/cr/ac/cenfotec/logica/Carta.java
@@ -4,9 +4,9 @@ public class Carta {
 	private String nombre;
 	private String palo;
 	private int valor;
-	
+
 	public Carta() {
-		
+
 	}
 
 	public String getNombre() {
@@ -29,9 +29,25 @@ public class Carta {
 		return valor;
 	}
 
-	public void setValor(int valor) {
-		this.valor = valor;
+	public void setValor(int valor) throws Exception {
+		if (validarValorCarta(valor))
+			this.valor = valor;
 	}
-	
-	
+
+	private boolean validarValorCarta(int valor) throws Exception {
+		if(valor >= 1 && valor <= 10) {
+			return true;
+		} else {
+			throw new Exception("El valor de la carta es incorrecta, no esta entre 1 al 10");
+		}
+	}
+
+	public boolean equals(Carta card) {
+		if (this.getValor() == card.getValor()) {
+			return true;
+		}
+
+		return false;
+	}
+
 }
--- Examen1Patrones/src/cr/ac/cenfotec/logica/test/CartaTest.java
+++ Examen1Patrones/src/cr/ac/cenfotec/logica/test/CartaTest.java
@@ -1,8 +1,40 @@
 package cr.ac.cenfotec.logica.test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import cr.ac.cenfotec.logica.Carta;
 
-class CartaTest {
-
+public class CartaTest {
+	
+	Carta cardA = new Carta();
+	Carta cardB = new Carta();
+	
+	public void modificarCarta(Carta a, String nombre, String palo, int valor) throws Exception {
+		a.setNombre(nombre);
+		a.setPalo(palo);
+		a.setValor(valor);
+	}
+	
+	@Test(expected = Exception.class)
+	public void testValor() throws Exception {
+		cardA = new Carta();
+		cardA.setValor(11);
+		assertEquals(11, cardA.getValor());
+	}
+	
+	@Test
+	public void testEquals() throws Exception {
+		modificarCarta(cardA, "3", "Escudos", 3);
+		modificarCarta(cardB, "3", "Estrellas", 3);
+		assertTrue(cardA.equals(cardB));
+	}
+	
+	@Test
+	public void testEquals1() throws Exception {
+		modificarCarta(cardA, "Jota", "Gotas", 10);
+		modificarCarta(cardB, "Ka", "Estrellas", 10);
+		assertTrue(cardA.equals(cardB));
+	}
 }
