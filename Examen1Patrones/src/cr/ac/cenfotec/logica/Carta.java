c88e232c36f5d2c13a45f03ad40a004a8d09c62f
stevenmontes <39229428+stevenmontes@users.noreply.github.com>
2018-06-15 16:40:00 -0600

    Implementa la clase Carta y CartaTest
    Se crea la clase de Carta y un junit test case llamado CartaTest, la carta contiene: nombre, palo, numero junto con sus setter and getter.

---
+++ Examen1Patrones/src/cr/ac/cenfotec/logica/Carta.java
@@ -0,0 +1,37 @@
+package cr.ac.cenfotec.logica;
+
+public class Carta {
+	private String nombre;
+	private String palo;
+	private int valor;
+	
+	public Carta() {
+		
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getPalo() {
+		return palo;
+	}
+
+	public void setPalo(String palo) {
+		this.palo = palo;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	
+	
+}
---
+++ Examen1Patrones/src/cr/ac/cenfotec/logica/test/CartaTest.java
@@ -0,0 +1,8 @@
+package cr.ac.cenfotec.logica.test;
+
+import org.junit.Test;
+import cr.ac.cenfotec.logica.Carta;
+
+class CartaTest {
+
+}
